3c18d73e944c57aa2efa0e6a69434b05a1219f7c
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2024-04-07 16:15:29 +0530

    Carreerz

--- src/components/Carreers.jsx
+++ src/components/Carreers.jsx
@@ -8,7 +8,7 @@ function Carreers() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-12">
         <div className="flex items-center flex-col justify-center border-r border-gray-300">
           <span className="italic font-serif text-lg text-center">&quot;Anyone who has never made a mistake has never tried anything new.&quot;</span>
-          <img className="max-w-[400px] h-[270px] rounded-lg shadow-lg hover:transition-all hover:-translate-y-2 hover:cursor-pointer my-3" src="assets/img15.png" alt="Join us!" />
+          <img className="max-w-[400px] h-[270px] rounded-[10px] shadow-lg hover:transition-all hover:-translate-y-2 hover:cursor-pointer my-3 px-2" src="assets/img15.png" alt="Join us!" />
         </div>
         <div className="flex flex-col justify-center items-center">
           <span className="text-lg text-center font-serif">Let&apos;s team up!</span>
