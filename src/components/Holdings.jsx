89c34f1bdb5a0792c9100452b8e392de8d5204d4
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2024-04-15 19:53:14 +0530

    timelinings

--- src/components/Holdings.jsx
+++ src/components/Holdings.jsx
@@ -3,27 +3,27 @@
 const Holdings = () => {
     // Array of random company names
     const companyNames = [
-        'Company A',
-        'Company B',
-        'Company C',
-        'Company D',
-        'Company E',
-        'Company F',
-        'Company G',
-        'Company H',
-        'Company I'
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
     ];
 
     return (
         <div className="mt-32">
             <h1 className="text-white text-3xl font-customFont ml-36 font-semibold">Holdings</h1>
         
-        <div className="flex flex-wrap justify-center p-4 mt-16">
+        <div className="flex flex-wrap justify-center p-4 mt-6">
             
             {companyNames.map((company, index) => (
                 <div
                     key={index}
-                    className="card w-72 h-[160px] md:w-1/4 bg-gray-400 hover:bg-gray-900 transition-colors duration-300 rounded-lg shadow-md m-4 flex justify-center items-center text-white text-center text-[18px] font-bold "
+                    className="card w-72 h-[160px] md:w-1/4 bg-gray-400 hover:bg-[#81D8D0] transition-colors duration-300 rounded-lg shadow-md m-4 flex justify-center items-center text-white text-center text-[18px] font-bold "
                 >
                     {company}
                 </div>
--- src/components/Philanthrophy.jsx
+++ src/components/Philanthrophy.jsx
@@ -2,25 +2,25 @@
 const Philanthrophy = () => {
     // Array of random company names
     const companyNames = [
-        'Charity A',
-        'Charity B',
-        'Charity C',
-        'Charity D',
-        'Charity E',
-        'Charity F',
-        'Charity G'
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
     ];
 
     return (
-        <div className="mt-32">
+        <div className="mt-36">
             
             <h1 className="text-white text-3xl font-customFont ml-36 font-semibold">Philanthrophy</h1>
 
-        <div className="flex flex-wrap justify-center p-4 mt-16">
+        <div className="flex flex-wrap justify-center p-4 mt-6">
             {companyNames.map((company, index) => (
                 <div
                     key={index}
-                    className="card w-72 h-[160px] md:w-1/4 bg-gray-500 hover:bg-gray-900 transition-colors duration-300 rounded-lg shadow-md m-4 flex justify-center items-center text-[18px] text-white text-center font-bold"
+                    className="card w-72 h-[160px] md:w-1/4 bg-gray-500 hover:bg-[#81D8D0] transition-colors duration-300 rounded-lg shadow-md m-4 flex justify-center items-center text-[18px] text-white text-center font-bold"
                 >
                     {company}
                 </div>
